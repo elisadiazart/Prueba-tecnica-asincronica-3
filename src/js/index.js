349ddee91a1ed421482e3d9bfc4fedd2de00920e
Elisa <elisadiazartcontact@gmail.com>
2023-02-02 20:55:54 +0100

    02.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
+import { root } from 'postcss';
 import '../scss/styles.scss';
 //import { sayHello } from './demo.js';
 
 const countryFlags = document.querySelectorAll('.card-flag');
 const countriesContainer = document.getElementById('countriesContainer');
+const themeMode = document.getElementById('theme-mode')
+const rootStyles = document.documentElement.style;
 
 const fetchData = async url => {
   const response = await fetch(url);
@@ -38,22 +41,29 @@ const printData = async () => {
     newRegionData.textContent = country.region;
     newRegionText.append(newRegionData);
 
-    // const newCapitalText = document.createElement('h3');
-    // newCapitalText.classList.add('card-text', 'card-text--bold');
-    // newCapitalText.textContent = 'Capital:';
-    // const newCapitalData = document.createElement('span');
-    // newCapitalData.classList.add('card-text');
-    // newCapitalData.textContent = country.capital && [...country.capital];
-    // newCapitalText.append(newRegionData);
+    const newCapitalText = document.createElement('h3');
+    newCapitalText.classList.add('card-text', 'card-text--bold');
+    newCapitalText.textContent = 'Capital:';
+    const newCapitalData = document.createElement('span');
+    newCapitalData.classList.add('card-text');
+    newCapitalData.textContent = country.capital && [...country.capital];
+    newCapitalText.append(newCapitalData);
     textContainer.append(newPopulationText);
     textContainer.append(newRegionText);
-    //textContainer.append(newCapitalText);
+    textContainer.append(newCapitalText);
     newCountry.append(cardFlag);
     newCountry.append(textContainer);
     fragment.append(newCountry);
-    console.log(country.region);
   });
   countriesContainer.append(fragment);
 };
 
 printData();
+
+themeMode.addEventListener('click', () => {
+  themeMode.dataset.mode = 'dark'
+  rootStyles.setProperty("--bg-color", "#2b3743")
+  rootStyles.setProperty("--secondary-bg", "rgba(32, 32, 32, 0.774)")
+  rootStyles.setProperty("--bg", "#202d36")
+  rootStyles.setProperty("--text-color", "white")
+})
